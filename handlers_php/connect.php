e9feb9e38dd9c392c2b37994084912cd517138fe
zerotodevs <romerodevs@gmail.com>
2025-03-21 10:08:23 +0800

    git commit -m "Add new resident"

--- handlers_php/connect.php
+++ handlers_php/connect.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="bms";
+$db="bms_database";
 $conn=mysqli_connect($host,$user,$pass,$db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
