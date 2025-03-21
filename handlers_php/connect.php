f36baec134ebbd50e16b83557a5c2309929e553a
zerotodevs <romerodevs@gmail.com>
2025-03-21 10:08:59 +0800

    git commit -m " "

--- handlers_php/connect.php
+++ handlers_php/connect.php
@@ -1,10 +1,16 @@
-<?php
-$host="localhost";
-$user="root";
-$pass="";
-$db="bms_database";
-$conn=mysqli_connect($host,$user,$pass,$db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}   
-?>
+    <?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "bms_database";
+
+    // Create connection
+    $conn = mysqli_connect($host, $user, $pass, $db);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        echo "Database connected successfully!";
+    }
+    ?>
